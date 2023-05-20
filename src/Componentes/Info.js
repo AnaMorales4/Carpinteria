1796de50a5a75e79dcb443142364ced54ffa26b9
AnaMorales4 <alt.anamor4@gmail.com>
2023-05-19 22:58:14 -0500

    css

--- src/Componentes/Info.js
+++ src/Componentes/Info.js
@@ -1,30 +1,30 @@
 import React from "react";
-function Informacion(){
-    return(
-<div class="contenedor">
-    <div class="mision" style={{justifyContent: "center", textAlign: 'center'}}>
-        <h1 style={{textAlign:"center"}}>MISIÓN</h1>
-        <p>Nuestra misión es crear muebles de alta calidad y diseño excepcional, utilizando habilidades 
-            artesanales y materiales sostenibles. <br/>
-            Nos esforzamos por ofrecer a nuestros clientes piezas únicas y duraderas que transformen espacios
-            y reflejen la belleza natural de la madera.</p>
-    </div>
-        <div>
-            <img src={require("../Imagenes/info1.jpg")} class="info1"/>
+function Informacion() {
+    return (
+        <div class="contenedor">
+            <div class="mision">
+                <h1>MISIÓN</h1>
+                <p>Nuestra misión es crear muebles de alta calidad y diseño excepcional, utilizando habilidades
+                    artesanales y materiales sostenibles. <br />
+                    Nos esforzamos por ofrecer a nuestros clientes piezas únicas y duraderas que transformen espacios
+                    y reflejen la belleza natural de la madera.</p>
+            </div>
+            <div>
+                <img src={require("../Imagenes/info1.jpg")} class="info1" />
+            </div>
+            <div class="vision">
+                <h1>VISIÓN</h1>
+                <p>Nuestra visión es convertirnos en la ebanistería de referencia, reconocida por nuestra artesanía
+                    meticulosa y nuestro compromiso con la excelencia. <br /> Buscamos establecer asociaciones duraderas con
+                    nuestros clientes, colaboradores y proveedores, y expandir nuestra presencia en el mercado,
+                    brindando soluciones personalizadas que superen las expectativas de quienes buscan muebles únicos y
+                    de calidad.
+                </p>
+            </div>
+            <div>
+                <img src={require("../Imagenes/info2.jpg")} class="info2" />
+            </div>
         </div>
-    <div class="vision" style={{justifyContent: "center", textAlign: 'center'}}>
-    <h1 style={{textAlign:"center"}}>VISIÓN</h1>
-    <p>Nuestra visión es convertirnos en la ebanistería de referencia, reconocida por nuestra artesanía
-         meticulosa y nuestro compromiso con la excelencia. <br/> Buscamos establecer asociaciones duraderas con
-         nuestros clientes, colaboradores y proveedores, y expandir nuestra presencia en el mercado,
-         brindando soluciones personalizadas que superen las expectativas de quienes buscan muebles únicos y
-        de calidad.
-    </p>
-    </div>
-        <div>
-            <img src={require("../Imagenes/info2.jpg")} class="info2"/>
-        </div>  
-</div>
     )
 }
 export default Informacion;
--- src/Componentes/Portafolio.js
+++ src/Componentes/Portafolio.js
@@ -1,38 +1,38 @@
 import React from 'react';
-function Portafoliof(){
+function Portafoliof() {
     return (
-<div className='Card'>
-<h4 class="Categorias">Closets</h4>
-    <div className='Card-body d-flex justify-content-center'>
-        <img src={require("../Imagenes/cajon.jpg")} class='col-md-3 border border-danger mx-auto m-2 '/>
-        <img src={require("../Imagenes/cajon2.jpg")} class='col-md-3 border border-danger mx-auto m-2'/>
-        <img src={require("../Imagenes/cajon3.jpg")} class='col-md-3 border border-danger mx-auto m-2'/>
-    </div>
-    <h4 class="Categorias">Salas</h4>
-    <div className='Card-body d-flex justify-content-center'>
-        <img src={require("../Imagenes/sala.jpg")} class='col-md-3 border border-info mx-auto m-2'/>
-        <img src={require("../Imagenes/sala2.jpg")} class='col-md-3 border border-info mx-auto m-2'/>
-        <img src={require("../Imagenes/sala3.jpg")} class='col-md-3 border border-info mx-auto m-2'/>
-    </div>
-    <h4 class="Categorias" >Cocinas</h4>
-    <div className='Card-body d-flex justify-content-center'>
-        <img src={require("../Imagenes/cocina.jpg")} class='col-md-3 border border-warning mx-auto m-2'/>
-        <img src={require("../Imagenes/cocina2.jpg")} class='col-md-3 border border-warning mx-auto m-2'/>
-        <img src={require("../Imagenes/cocina3.jpg")} class='col-md-3 border border-warning mx-auto m-2'/>
-    </div>
-    <h4 class="Categorias">Escritorios</h4>
-    <div className='Card-body d-flex justify-content-center'>
-        <img src={require("../Imagenes/escritorio.jpg")} class='col-md-3 border border-primary mx-auto m-2'/>
-        <img src={require("../Imagenes/escritorio2.jpg")} class='col-md-3 border border-primary mx-auto m-2'/>
-        <img src={require("../Imagenes/escritorio3.jpg")} class='col-md-3 border border-primary mx-auto m-2'/>
-    </div>
-    <h4 class="Categorias">Tocadores</h4>
-    <div className='Card-body d-flex justify-content-center'>
-        <img src={require("../Imagenes/tocador.jpg")} class='col-md-3 border border-success mx-auto m-2'/>
-        <img src={require("../Imagenes/tocador3.jpg")} class='col-md-3 border border-success mx-auto m-2'/>
-        <img src={require("../Imagenes/tocador2.jpg")} class='col-md-3 border border-success mx-auto m-2'/>
-    </div>
-</div>
+        <div className='Card'>
+            <h4 class="Categorias">Closets</h4>
+            <div className='Card-body d-flex justify-content-center'>
+                <img src={require("../Imagenes/cajon.jpg")} class='col-md-3 border border-danger mx-auto m-2 ' />
+                <img src={require("../Imagenes/cajon2.jpg")} class='col-md-3 border border-danger mx-auto m-2' />
+                <img src={require("../Imagenes/cajon3.jpg")} class='col-md-3 border border-danger mx-auto m-2' />
+            </div>
+            <h4 class="Categorias">Salas</h4>
+            <div className='Card-body d-flex justify-content-center'>
+                <img src={require("../Imagenes/sala.jpg")} class='col-md-3 border border-info mx-auto m-2' />
+                <img src={require("../Imagenes/sala2.jpg")} class='col-md-3 border border-info mx-auto m-2' />
+                <img src={require("../Imagenes/sala3.jpg")} class='col-md-3 border border-info mx-auto m-2' />
+            </div>
+            <h4 class="Categorias" >Cocinas</h4>
+            <div className='Card-body d-flex justify-content-center'>
+                <img src={require("../Imagenes/cocina.jpg")} class='col-md-3 border border-warning mx-auto m-2' />
+                <img src={require("../Imagenes/cocina2.jpg")} class='col-md-3 border border-warning mx-auto m-2' />
+                <img src={require("../Imagenes/cocina3.jpg")} class='col-md-3 border border-warning mx-auto m-2' />
+            </div>
+            <h4 class="Categorias">Escritorios</h4>
+            <div className='Card-body d-flex justify-content-center'>
+                <img src={require("../Imagenes/escritorio.jpg")} class='col-md-3 border border-primary mx-auto m-2' />
+                <img src={require("../Imagenes/escritorio2.jpg")} class='col-md-3 border border-primary mx-auto m-2' />
+                <img src={require("../Imagenes/escritorio3.jpg")} class='col-md-3 border border-primary mx-auto m-2' />
+            </div>
+            <h4 class="Categorias">Tocadores</h4>
+            <div className='Card-body d-flex justify-content-center'>
+                <img src={require("../Imagenes/tocador.jpg")} class='col-md-3 border border-success mx-auto m-2' />
+                <img src={require("../Imagenes/tocador3.jpg")} class='col-md-3 border border-success mx-auto m-2' />
+                <img src={require("../Imagenes/tocador2.jpg")} class='col-md-3 border border-success mx-auto m-2' />
+            </div>
+        </div>
     )
 }
 export default Portafoliof
