eb06e67b7573928f68c089dc1cdea8693a20e040
AnaMorales4 <alt.anamor4@gmail.com>
2023-05-27 22:25:49 -0500

    pedidos

--- src/Componentes/Consulta.js
+++ src/Componentes/Consulta.js
@@ -1,7 +1,21 @@
 import React from "react";
-function ConsultaPedido(){
-    return(
-        <h1>hloas</h1>
+function ConsultaPedido() {
+    return (
+        <div>
+            <form class="row g-3">
+                <div class="col-auto">
+                    <label for="staticEmail2" class="visually-hidden">Email</label>
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Número de pedido:"/>
+                </div>
+                <div class="col-auto">
+                    <label for="inputPassword2" class="visually-hidden">Password</label>
+                    <input type="password" class="form-control" id="inputPassword2" placeholder="Ingresar número"/>
+                </div>
+                <div class="col-auto">
+                    <button class="enviar" style={{color:'white'}}>Confirmar pedido</button>
+                </div>
+            </form>
+        </div>
     )
 }
 export default ConsultaPedido;
--- src/Componentes/Pedidos.js
+++ src/Componentes/Pedidos.js
@@ -1,7 +1,64 @@
 import React from "react";
-function Ordenes(){
-    return(
- <h1>hpla</h1>
+function Ordenes() {
+    return (
+        <div>
+            <div>
+                <form class="row g-3">
+                    <div class="col-md-5">
+                        <label class="form-label">Nombre:</label>
+                        <input type="text" class="form-control" value="Ingresar nombre" />
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Apellido:</label>
+                        <input type="text" class="form-control" value="Ingresar apellido" />
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Ciudad:</label>
+                        <input type="text" class="form-control" value="Ingresar cuidad" />
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Dirección:</label>
+                        <input type="text" class="form-control" value="Ingresar dirección" />
+                    </div>
+                    <div>
+                        <select class="form-select;col-md-10" aria-label="Default select example">
+                            <option selected>Tipo de servicio</option>
+                            <option value="1">Closet</option>
+                            <option value="2">Tocador</option>
+                            <option value="3">Cajones para la cocina</option>
+                            <option value="4">Cajones para la sala</option>
+                            <option value="5">Escritorios</option>
+                            <option value="6">Otro</option>
+                        </select>
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Medida ancho:</label>
+                        <input type="text" class="form-control" value="Ingresar ancho" />
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Medida largo:</label>
+                        <input type="text" class="form-control" value="Ingresar alto" />
+                    </div>
+                    <div class="col-md-5">
+                        <label class="form-label">Especificaciones:</label>
+                        <input type="text" class="form-control" value="Ej: cantidad puertas" />
+                    </div>
+                    <div>
+                        <select class="form-select;col-md-10" aria-label="Default select example">
+                            <option selected>Tipo de material</option>
+                            <option value="1">Madera</option>
+                            <option value="2">Maple</option>
+                            <option value="3">Pino</option>
+                            <option value="4">Melanina</option>
+                        </select>
+                    </div>
+                    <div class="col-12">
+                        <button class="enviar" style={{color:'white'}}>Enviar pedido</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
     )
 }
 export default Ordenes;
