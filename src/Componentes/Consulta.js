38ec196774b874dfb33be9bba3b50f6010c5025c
AnaMorales4 <alt.anamor4@gmail.com>
2023-05-28 21:41:25 -0500

    asdf

--- src/Componentes/Consulta.js
+++ src/Componentes/Consulta.js
@@ -1,8 +1,8 @@
 import React from "react";
 function ConsultaPedido() {
     return (
-        <div>
-            <form class="row g-3">
+        <div class="row">
+            <form class="row g-3 justify-content-center">
                 <div class="col-auto">
                     <label for="staticEmail2" class="visually-hidden">Email</label>
                     <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Número de pedido:"/>
@@ -12,7 +12,7 @@ function ConsultaPedido() {
                     <input type="password" class="form-control" id="inputPassword2" placeholder="Ingresar número"/>
                 </div>
                 <div class="col-auto">
-                    <button class="enviar" style={{color:'white'}}>Confirmar pedido</button>
+                    <button class="consultap" style={{color:'white'}}>Confirmar pedido</button>
                 </div>
             </form>
         </div>
