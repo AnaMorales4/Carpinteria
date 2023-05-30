b8aa2d6d8dad4abd9f453e96e5b87fc0d6d64a27
AnaMorales4 <alt.anamor4@gmail.com>
2023-05-29 21:03:07 -0500

    asd

--- src/Componentes/Pedidos.js
+++ src/Componentes/Pedidos.js
@@ -28,7 +28,7 @@ function Ordenes() {
                 .then(() => {
                     alert("Producto creado");
                     limpiarCampos();
-                }).catch(()=>{
+                }).catch(() => {
                     alert("Error")
                 });
         }
@@ -55,49 +55,51 @@ function Ordenes() {
                 </div>
                 <div class="col-md-5">
                     <label class="form-label">Apellido:</label>
-                    <input type="text" class="form-control" placeholder="Ingresar apellido" onChange={(event)=>{
-                          setApellido(event.target.value)
-                        }}/>
+                    <input type="text" class="form-control" placeholder="Ingresar apellido" onChange={(event) => {
+                        setApellido(event.target.value)
+                    }} />
                 </div>
                 <div class="col-md-5">
                     <label class="form-label">Ciudad:</label>
-                    <input type="text" class="form-control" placeholder="Ingresar cuidad"onChange={(event)=>{
-                          setCiudad(event.target.value)
-                        }} />
+                    <input type="text" class="form-control" placeholder="Ingresar cuidad" onChange={(event) => {
+                        setCiudad(event.target.value)
+                    }} />
                 </div>
                 <div class="col-md-5">
                     <label class="form-label">Dirección:</label>
-                    <input type="text" class="form-control" placeholder="Ingresar dirección"onChange={(event)=>{
-                          setDireccion(event.target.value)
-                        }} />
+                    <input type="text" class="form-control" placeholder="Ingresar dirección" onChange={(event) => {
+                        setDireccion(event.target.value)
+                    }} />
                 </div>
                 <div class="col-md-5">
-                <label class="form-label">Tipo de servicio:</label>
-                    <input type="text" class="form-control" placeholder="Tocador, Cajon cocina, Closet, etc..."onChange={(event)=>{
-                          setTiposervicio(event.target.value)}}></input>
+                    <label class="form-label">Tipo de servicio:</label>
+                    <input type="text" class="form-control" placeholder="Tocador, Cajon cocina, Closet, etc..." onChange={(event) => {
+                        setTiposervicio(event.target.value)
+                    }}></input>
                 </div>
                 <div class="col-md-5">
-                <label class="form-label">Material:</label>
-                      <input type="text" class="form-control" placeholder="Madera, Laurel, Pino, Laminas RH, etc..."onChange={(event)=>{
-                          setMaterial(event.target.value)}}></input>
+                    <label class="form-label">Material:</label>
+                    <input type="text" class="form-control" placeholder="Madera, Laurel, Pino, Laminas RH, etc..." onChange={(event) => {
+                        setMaterial(event.target.value)
+                    }}></input>
                 </div>
                 <div class="col-md-5">
                     <label class="form-label">Medida ancho:</label>
-                    <input type="text" class="form-control" placeholder="Ingresar ancho" onChange={(event)=>{
-                          setAncho(event.target.value)
-                        }}/>
+                    <input type="text" class="form-control" placeholder="Ingresar ancho" onChange={(event) => {
+                        setAncho(event.target.value)
+                    }} />
                 </div>
                 <div class="col-md-5">
                     <label class="form-label">Medida largo:</label>
-                    <input type="text" class="form-control" placeholder="Ingresar largo"onChange={(event)=>{
-                          setLargo(event.target.value)
-                        }} />
+                    <input type="text" class="form-control" placeholder="Ingresar largo" onChange={(event) => {
+                        setLargo(event.target.value)
+                    }} />
                 </div>
                 <div class="col-md-10">
                     <label class="form-label">Especificaciones:</label>
-                    <input type="text" class="form-control" placeholder="Ej: cantidad puertas, tipo de bisagras..." onChange={(event)=>{
-                          setEspecificaciones(event.target.value)
-                        }}/>
+                    <input type="text" class="form-control" placeholder="Ej: cantidad puertas, tipo de bisagras..." onChange={(event) => {
+                        setEspecificaciones(event.target.value)
+                    }} />
                 </div>
                 <div class="col-12 text-center">
                     <button class="enviar" style={{ color: 'white' }} type="submit" onClick={createPedidos}>Enviar pedido</button>
